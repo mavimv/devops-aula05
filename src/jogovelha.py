5d03534f3791801deb715d2d6509a643d6ff37ba
mavimv <60761632+mavimv@users.noreply.github.com>
2020-04-17 21:56:05 -0300

    versão 1

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,16 +1,15 @@
 def inicializar():
-    tab = [ ]
+    tab = []
     for i in range(3):
-        linha = [ ]
+        linha = []
         for j in range(3):
             linha.append(".")
         tab.append(linha)
     return tab
 
-def main( ):
-    jogo = inicializar( )
+def main():
+    jogo = inicializar()
     print (jogo)
 
 if __name__ == "__main__":
-
-main()
+    main()
